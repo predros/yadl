aa6365ffea9f3f76f0088d82dd9e5f71c8ff1d50
Pedro Fialho <pedro.h.fialho@outlook.com>
2023-08-09 18:51:06 -0300

    Added a hacky way to handle Woof's verbose stderr output.

--- src/views/mainwindow/mainwindow.cpp
+++ src/views/mainwindow/mainwindow.cpp
@@ -150,11 +150,168 @@ void MainWindow::launch(QString port_path, SourcePortType port_type, QString iwa
     }
 
     run_process.start(port_command, args_list);
-    run_process.waitForFinished();
+    run_process.waitForFinished(-1);
 
     QString error(run_process.readAllStandardError());
 
-    if (!error.trimmed().isEmpty())
+    // Woof has a really annoying habit of outputting everything
+    // to stderr instead of stdoutput
+    if (port_type == SOURCEPORT_WOOF) {
+        // Actual breaking errors (I_Error)
+        static QStringList valid_errors = {
+            "IWAD not found",
+            "Unknown or invalid IWAD file",
+            "Unknown game version",
+            "Invalid parameter",
+            "No parameter for",
+            "Bad savegame",
+            "Couldn't read file",
+            "Error: couldn't open",
+
+            "CheckIWAD: failed to read IWAD",
+            "CheckIWAD: failed to read header",
+            "CheckIWAD: IWAD tag not present",
+            "CheckIWAD: failed to read directory",
+
+            "check your dehacked",
+            "Check your dehacked",
+            "Error reading lump directory from",
+            "Disconnected from server in drone mode",
+            "Lost connection to server",
+            "Netgame startup aborted",
+            "D_StartNetGame: invalid ticdup value",
+            "No server found on local LAN",
+            "Unable to resolve",
+            "D_InitNetGame: Failed to connect to",
+            "TryRunTics: lowtic < gametic",
+            "gametic>lowtic",
+            "D_AddZipFile: Failed to open",
+            "No such response file!",
+            "Cannot find .deh or .bex file named",
+            "'BETAGRPH.WAD' is required for beta emulation!",
+            "You cannot -file with the shareware version",
+            "This is not the registered version",
+            "Not enough players to continue the demo",
+            "G_DoPlayDemo: Unknown demo format",
+            "consistency failure",
+            "G_CheckSpot: unexpected angle",
+            "G_ReloadDefaults: Beta emulation requires complevel MBF",
+            "G_ReloadDefaults: Helper dogs require complevel MBF or MBF21",
+            "mbf21_WriteOptions: MBF21_GAME_OPTION_SIZE is too small",
+            "G_WriteOptions: GAME_OPTION_SIZE is too small",
+            "Encountered unknown mbf21 compatibility options",
+            "The -beta option is required to play",
+            "Error recording demo",
+            "I_UpdateSoundParams: channel out of range",
+            "I_StopSound: channel out of range",
+            "I_SoundIsPlaying: channel out of range",
+            "I_SoundID: channel out of range",
+            "MidiPlayerFallback: No music module could be initialized",
+            "I_Realloc: failed on reallocation",
+            "Failed to get renderer output size",
+            "Could not get display mode for video display",
+            "Error creating window for video startup",
+            "Error creating window for video startup",
+            "Error creating renderer for screen window",
+            "Failed to initialize video",
+            "Failed to allocate new string",
+            "Could not find config variable",
+            "Could not write defaults to",
+            "Failed to duplicate string",
+            "Failed to open",
+            "NET_AddModule: No more modules for context",
+            "NET_CL_InitServer: attempted to initialize client pipe end as a server",
+            "NET_SV_InitClient: attempted to initialize server pipe end as a client",
+            "NET_QueryAddress: Host",
+            "No response from",
+            "NET_SDL_FreeAddress: Attempted to remove an unused address",
+            "NET_SDL_InitClient: Unable to open a socket",
+            "NET_SDL_InitServer: Unable to bind to port",
+            "NET_SDL_SendPacket: Error transmitting packet",
+            "NET_SDL_RecvPacket: Error receiving packet",
+            "Wanted to send",
+            "NET_WriteProtocol: protocol",
+            "Weird actor->movedir",
+            "P_SetupLevel: Hexen map format not supported in",
+            "P_LoadNodes: Error during ZDBSP nodes decompression initialization",
+            "P_LoadNodes: Error during ZDBSP nodes decompression",
+            "P_LoadNodes: Error during ZDBSP nodes decompression shut-down",
+            "P_LoadNodes: No subsectors in map",
+            "P_LoadNodes: Incorrect number of segs in ZDBSP nodes",
+            "P_GiveAmmo: bad type",
+            "PTR_SlideTraverse: not a line",
+            "P_MapStart: tmthing set",
+            "Unknown tclass",
+            "P_UnArchiveSpecials: Pusher thinker without source in sector",
+            "P_UnarchiveSpecials:Unknown tclass",
+            "P_CrossSubsector: ss",
+            "P_InitPicAnims: bad cycle from",
+            "P_StartButton: no button slots left",
+            "R_Subsector: ss",
+            "R_InitTextures: bad texture directory",
+            "R_DrawColumn: ",
+            "R_DrawSkyColumn: ",
+            "R_DrawFuzzColumn: ",
+            "R_MapPlane: ",
+            "Bad R_RenderWallRange",
+            "R_InstallSpriteLump: Bad frame characters in lump",
+            "R_DrawSpriteRange: bad texturecolumn",
+            "invalid sprite number",
+            "invalid frame",
+            "Bad sfx",
+            "Attempt to set music volume at",
+            "Attempt to set sfx volume at",
+            "Bad music number",
+            "W_FileLength: failure in fstat",
+            "W_InitFiles: no files found",
+            "Couldn't allocate lumpcache",
+            "Cannot open internal lumps wad",
+            "owner is required for purgable blocks",
+            "Z_Malloc: Failure trying to allocate",
+            "freed a pointer without ZONEID",
+        };
+
+        static QList<QRegularExpression> valid_regex = {
+            QRegularExpression("IWAD file .* not found"),
+            QRegularExpression("Wad file .* doesn't have IWAD or PWAD id"),
+            QRegularExpression("Only .* deathmatch spots, .* required"),
+            QRegularExpression("The command line parameter .* was "
+                               "specified to a dedicated server."),
+            QRegularExpression("P_LoadSegs: linedef .* for seg .*"),
+            QRegularExpression("Line .* is missing a tag"),
+            QRegularExpression("[0-9]* errors"),
+            QRegularExpression("R_InitSprites: Sprite .* frame .* "
+                               "is missing rotations"),
+            QRegularExpression("handle .* out of range"),
+            QRegularExpression("Bad V_.*"),
+            QRegularExpression("W_GetNumForName: .* not found"),
+            QRegularExpression(".* >= numlumps"),
+            QRegularExpression("Z_FreeTag: Tag .* does not exist"),
+
+
+        };
+
+        QStringList errors = error.split('\n');
+
+        auto iter = errors.constEnd();
+        bool found_error = false;
+
+        while (iter != errors.constBegin() && !found_error) {
+            iter--;
+
+            for (auto& regex : valid_regex) {
+                if (regex.match(*iter).hasMatch()) {
+                    throw *iter;
+                }
+            }
+
+            for (auto& string : valid_errors) {
+                if (iter->contains(string)) {
+                    throw *iter;
+                }
+            }
+        }
+    } else if (!error.trimmed().isEmpty())
         throw error.remove('\n');
 }
 
